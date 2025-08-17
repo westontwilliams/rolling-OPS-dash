8fa3effa470765b5e1876f6f3247e936398093df
williamsw21 <westontwilliams25@gmail.com>
2025-08-17 03:13:04 -0400

    Selecting a team filters to all players of that team

--- streaky_tracker.R
+++ streaky_tracker.R
@@ -7,22 +7,18 @@ hitters_api <- "https://www.fangraphs.com/api/leaders/major-league/data?age=&pos
 r <- GET(hitters_api)
 hitters <- fromJSON(content(r, as = "text"))
 hitters <- as.data.frame(hitters)
-hitters_final <- hitters %>%
-  select(data.PlayerName, data.TeamName, data.playerid, data.teamid)
 
 teams_api <- "https://www.fangraphs.com/api/leaders/major-league/data?age=&pos=all&stats=bat&lg=all&qual=1&season=2025&season1=2025&startdate=2025-03-01&enddate=2025-11-01&month=0&hand=&team=0%2Cts&pageitems=2000000000&pagenum=1&ind=0&rost=0&players=&type=8&postseason=&sortdir=asc&sortstat=TeamNameAbb"
 r <- GET(teams_api)
 teams <- fromJSON(content(r, as = "text"))
 teams <- as.data.frame(teams)
-teams_final <- teams %>% 
-  select(data.TeamName, data.teamid)
 
 ui <- fluidPage(
   titlePanel("Select an MLB Team"),
   sidebarLayout(
     sidebarPanel(
-      selectInput("team", "Choose a Team:", choices = c("", teams_final$data.TeamName)),
-      selectInput("player", "Choose a Player:", choices = c("", hitters_final$data.PlayerName))
+      selectInput("team", "Choose a Team:", choices = c("", teams$data.TeamName)),
+      selectInput("player", "Choose a Player:", choices = c("", hitters$data.PlayerName))
     ),
     mainPanel(
       textOutput("selection")
@@ -30,7 +26,22 @@ ui <- fluidPage(
   )
 )
 
-server <- function(input, output) {
+server <- function(input, output, session) {
+  filtered_players <- reactive({
+    req(input$team)
+    tid <- teams$data.teamid[teams$data.TeamName == input$team]
+    
+    hitters %>%
+      filter(data.teamid == tid) %>%
+      pull(data.PlayerName)
+  })
+  
+  observe({
+    updateSelectInput(session, "player",
+                      choices = c("", filtered_players()),
+                      selected = "")
+  })
+  
   output$selection <- renderText({
     req(input$team, input$player)
     paste0("You selected: ", input$team, ", ", input$player)
