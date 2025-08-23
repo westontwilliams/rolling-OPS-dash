262561749b50a93726e98bdf868f814111958802
williamsw21 <westontwilliams25@gmail.com>
2025-08-23 15:41:45 -0400

    added jsonlite package

--- app.R
+++ app.R
@@ -6,6 +6,7 @@ library(DT)
 library(ggplot2)
 library(zoo)
 library(httr)
+library(jsonlite)
 
 hitters_api <- "https://www.fangraphs.com/api/leaders/major-league/data?age=&pos=all&stats=bat&lg=all&qual=1&season=2025&season1=2025&startdate=2025-03-01&enddate=2025-11-01&month=0&hand=&team=0%2Cto&pageitems=2000000000&pagenum=1&ind=0&rost=0&players=&type=8&postseason=&sortdir=default&sortstat=PlayerName"
 r <- GET(hitters_api)
